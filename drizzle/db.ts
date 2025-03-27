522ff170fca176e90bd7a2d48af1fae042e3327c
Sadman Kibria <sadmankibria1@gmail.com>
2025-03-27 12:42:23 +0000

    created type safe for database url

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,5 +1,6 @@
+import { env } from "@/app/data/env/server";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const sql = neon(process.env.DATABASE_URL as string);
+const sql = neon(env.DATABASE_URL);
 export const db = drizzle({ client: sql });
