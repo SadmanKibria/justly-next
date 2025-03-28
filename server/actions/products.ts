811444fb924c8fc165217212bd443641b00b6995
Sadman Kibria <sadmankibria1@gmail.com>
2025-03-28 22:53:11 +0000

    New product form completed, product grid created, modal created for product. Caching almost done.

---
+++ server/actions/products.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { productDetailsSchema } from "@/schemas/products";
+import { auth } from "@clerk/nextjs/server";
+import { z } from "zod";
+import {
+  createProduct as createProductDb,
+  deleteProduct as deleteProductDb,
+} from "@/server/db/products";
+import { redirect } from "next/navigation";
+
+export async function createProduct(
+  unsafeData: z.infer<typeof productDetailsSchema>
+): Promise<{ error: boolean; message: string } | undefined> {
+  const { userId } = auth();
+  const { success, data } = productDetailsSchema.safeParse(unsafeData);
+  if (!success || userId == null) {
+    return { error: true, message: "Invalid data" };
+  }
+
+  const { id } = await createProductDb({ ...data, clerkUserId: userId });
+
+  redirect(`/dashboard/products/${id}/edit?tab=countries`);
+}
+
+export async function deleteProduct(id: string) {
+  const { userId } = auth();
+  const errorMessage = "There was an error deleting your product";
+
+  if (userId == null) {
+    return { error: true, message: errorMessage };
+  }
+
+  const isSuccess = await deleteProductDb({ id, userId });
+
+  return {
+    error: !isSuccess,
+    message: isSuccess ? "Successfully deleted your product" : errorMessage,
+  };
+}
