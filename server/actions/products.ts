d0d2299ad0ccfab19ddb8f75016edb9dee811b23
Sadman Kibria <sadmankibria1@gmail.com>
2025-03-29 10:36:23 +0000

    Caching completed and edit product details form completed

--- server/actions/products.ts
+++ server/actions/products.ts
@@ -6,6 +6,7 @@ import { z } from "zod";
 import {
   createProduct as createProductDb,
   deleteProduct as deleteProductDb,
+  updateProduct as updateProductDb,
 } from "@/server/db/products";
 import { redirect } from "next/navigation";
 
@@ -14,13 +15,35 @@ export async function createProduct(
 ): Promise<{ error: boolean; message: string } | undefined> {
   const { userId } = auth();
   const { success, data } = productDetailsSchema.safeParse(unsafeData);
-  if (!success || userId == null) {
-    return { error: true, message: "Invalid data" };
+  const canCreate = await canCreateProduct(userId);
+
+  if (!success || userId == null || !canCreate) {
+    return { error: true, message: "There was an error creating your product" };
   }
 
   const { id } = await createProductDb({ ...data, clerkUserId: userId });
 
   redirect(`/dashboard/products/${id}/edit?tab=countries`);
+}
+
+export async function updateProduct(
+  id: string,
+  unsafeData: z.infer<typeof productDetailsSchema>
+): Promise<{ error: boolean; message: string } | undefined> {
+  const { userId } = auth();
+  const { success, data } = productDetailsSchema.safeParse(unsafeData);
+  const errorMessage = "There was an error updating your product";
+
+  if (!success || userId == null) {
+    return { error: true, message: errorMessage };
+  }
+
+  const isSuccess = await updateProductDb(data, { id, userId });
+
+  return {
+    error: !isSuccess,
+    message: isSuccess ? "Product details updated" : errorMessage,
+  };
 }
 
 export async function deleteProduct(id: string) {
--- server/db/products.ts
+++ server/db/products.ts
@@ -3,16 +3,26 @@ import { ProductCustomizationTable, ProductTable } from "@/drizzle/schema";
 import {
   CACHE_TAGS,
   dbCache,
+  getIdTag,
   getUserTag,
   revalidateDbCache,
 } from "@/lib/cache";
 import { and, eq } from "drizzle-orm";
+import { get } from "http";
 
 export function getProducts(userId: string, { limit }: { limit?: number }) {
+  //return getProductsInternal(userId, { limit });
   const cacheFn = dbCache(getProductsInternal, {
     tags: [getUserTag(userId, CACHE_TAGS.products)],
   });
   return cacheFn(userId, { limit });
+}
+
+export function getProduct({ id, userId }: { id: string; userId: string }) {
+  const cacheFn = dbCache(getProductInternal, {
+    tags: [getIdTag(id, CACHE_TAGS.products)],
+  });
+  return cacheFn({ id, userId });
 }
 
 export async function createProduct(data: typeof ProductTable.$inferInsert) {
@@ -39,6 +49,26 @@ export async function createProduct(data: typeof ProductTable.$inferInsert) {
   });
 
   return newProduct;
+}
+
+export async function updateProduct(
+  data: Partial<typeof ProductTable.$inferInsert>,
+  { id, userId }: { id: string; userId: string }
+) {
+  const { rowCount } = await db
+    .update(ProductTable)
+    .set(data)
+    .where(and(eq(ProductTable.clerkUserId, userId), eq(ProductTable.id, id)));
+
+  if (rowCount > 0) {
+    revalidateDbCache({
+      tag: CACHE_TAGS.products,
+      userId,
+      id,
+    });
+  }
+
+  return rowCount > 0;
 }
 
 export async function deleteProduct({
@@ -68,5 +98,12 @@ function getProductsInternal(userId: string, { limit }: { limit?: number }) {
     where: ({ clerkUserId }, { eq }) => eq(clerkUserId, userId),
     orderBy: ({ createdAt }, { desc }) => desc(createdAt),
     limit,
+  });
+}
+
+function getProductInternal({ id, userId }: { id: string; userId: string }) {
+  return db.query.ProductTable.findFirst({
+    where: ({ clerkUserId, id: idCol }, { eq, and }) =>
+      and(eq(clerkUserId, userId), eq(idCol, id)),
   });
 }
