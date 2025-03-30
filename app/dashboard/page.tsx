28677cdfdbb2b858b23eade94e3921f6f5d0b2f5
Sadman Kibria <sadmankibria1@gmail.com>
2025-03-30 21:36:55 +0100

    Subscription page completed and Stripe integrated

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,10 @@
 import { getProducts } from "@/server/db/products";
 import { auth } from "@clerk/nextjs/server";
 import { NoProducts } from "./_components/NoProducts";
+import Link from "next/link";
+import { ArrowRightIcon, PlusIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { ProductGrid } from "./_components/ProductGrid";
 
 export default async function DashboardPage() {
   const { userId, redirectToSignIn } = auth();
@@ -10,5 +14,36 @@ export default async function DashboardPage() {
 
   if (products.length === 0) return <NoProducts />;
 
-  return null;
+  return (
+    <>
+      <h2 className="mb-6 text-3xl font-semibold flex justify-between">
+        <Link
+          className="group flex gap-2 items-center hover:underline"
+          href="/dashboard/products"
+        >
+          Products
+          <ArrowRightIcon className="group-hover:translate-x-1 transition-transform" />
+        </Link>
+        <Button asChild>
+          <Link href="/dashboard/products/new">
+            <PlusIcon className="size-4 mr-2" />
+            New Product
+          </Link>
+        </Button>
+      </h2>
+      <ProductGrid products={products} />
+      <h2 className="mb-6 text-3xl font-semibold flex justify-between mt-12">
+        <Link
+          href="/dashboard/analytics"
+          className="flex gap-2 items-center hover:underline group"
+        >
+          Analytics
+          <ArrowRightIcon className="group-hover:translate-x-1 transition-transform" />
+        </Link>
+      </h2>
+      <HasPermission permission={canAccessAnalytics} renderFallback>
+        <AnalyticsChart userId={userId} />
+      </HasPermission>
+    </>
+  );
 }
---
+++ server/db/productViews.ts
@@ -0,0 +1,27 @@
+import { db } from "@/drizzle/db";
+import { ProductTable, ProductViewTable } from "@/drizzle/schema";
+import { eq, and, gte, count } from "drizzle-orm";
+import { CACHE_TAGS, dbCache, getUserTag } from "@/lib/cache";
+
+export function getProductViewCount(userId: string, startDate: Date) {
+  const cacheFn = dbCache(getProductViewCountInternal, {
+    tags: [getUserTag(userId, CACHE_TAGS.productViews)],
+  });
+
+  return cacheFn(userId, startDate);
+}
+
+async function getProductViewCountInternal(userId: string, startDate: Date) {
+  const counts = await db
+    .select({ pricingViewCount: count() })
+    .from(ProductViewTable)
+    .innerJoin(ProductTable, eq(ProductTable.id, ProductViewTable.productId))
+    .where(
+      and(
+        eq(ProductTable.clerkUserId, userId),
+        gte(ProductViewTable.visitedAt, startDate)
+      )
+    );
+
+  return counts[0]?.pricingViewCount ?? 0;
+}
